1aaee9b1478be05402b71f926d13789adfe21081
noukpoaimee <noukpoaimee@gmail.com>
2020-09-26 20:44:40 +0100

    javascript

---
+++ main.js
@@ -0,0 +1,17 @@
+//pratique1
+let name = "toto";
+console.log(name);
+
+//pratique2
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let cars = ["toyota", "volvo", "renault"];
+let success = true;
+bigArray = [cars, numbers, success];
+console.log(bigArray);
+
+//pratique3
+
+function carre(a=5) {
+    let carree = a*a;
+    console.log(carree);
+}carre(a=5);
